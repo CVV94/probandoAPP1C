433400ced6fa3a01b814074f473903bdb8803610
Carlos vasquez <enrique-vasquez1@outlook.es>
2023-05-06 23:37:47 -0400

    Se guarda hasta base de datos, creacion y superusuario

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User             #importacion de una tabla modelo de django
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.TextField(blank=True)           #texto mas largo
+    created=models.DateTimeField(auto_now_add=True)     #se rellena automaticamente las fechas
+    datecompleted= models.DateTimeField(null=True)      #hay que determinar la fecha
+    important=models.BooleanField(default=False)        #por defecto la tarea no es importante
+    user= models.ForeignKey(User, on_delete=models.CASCADE)                       #hace llamdo a otra tabla User, se tiene que importar # eliminacion en cascada
+
+    def __str__(self) -> str:
+        return self.title
+
+    #crear superusuario
+    #py manage.py createsuperuser
+
+    #guardar migracion
+    #py manage.py makemigrations
+    #py manage.py migrate
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,65 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+# render nativo y redirect es para redireccionar
+from django.shortcuts import render, redirect                         	# creacion de un login  , autentificacion despues del registro
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User  							# guarda datos   			
+from django.contrib.auth import login, logout, authenticate				# crea un autoguardado de los registros(registro,salir,autentificar logeo)
+from django.http import HttpResponse  									# imprime mensajes
 
-def helloworld(request):
-	return render(request, 'signup.html',{
-		'form':UserCreationForm
-	})
+
+def home(request):								#index
+    return render(request, 'home.html')
+
+
+def signup(request):							#Registrarse
+
+    if request.method == 'GET':              # Get es para enviar formulario y Post Recibe formulario
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # registro
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)  # guarda el user  antes de redireccionar
+                return redirect('tasks')  # guarda el registro
+
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Usuario ya existente',
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password no coincide'
+        })
+
+
+def tasks(request):												#Tareas
+    return render(request, 'tasks.html')
+
+
+def signout(request):											#Cerrar sesion
+    logout(request)  # logout es una importacion que se sale de una sesion
+    return redirect('home')  # es una redireccion
+
+    
+def signin(request):											#iniciar sesion
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html',{
+                'form': AuthenticationForm,
+                'error':'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks') # es una redireccion
+
+
