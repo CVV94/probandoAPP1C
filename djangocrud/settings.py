c11d09aba28751a13ddacf5af337c0d6c677802f
Carlos vasquez <enrique-vasquez1@outlook.es>
2023-05-11 01:48:54 -0400

    Modificacion de Aplicacion para subirlo a la nube

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os                                        #importacion para renderizar
+# Don't forget to import dj-database-url at the beginning of the file
+              
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i#^zf&6@=i&dstfpx-3g8k8r*a51uq1i^5d&sla-#_wvmn@fi_'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')     #renderizacion con render .....................................
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ                                          #usar el dbug de render
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')       #Render ip automatica y ademas instalar el comando para guardar base de datos de render, pip install  DJ-Database-URL 
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -48,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
@@ -74,11 +84,12 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+DATABASES = {                                                                       #base de datos...............................
+    'default': dj_database_url.config(                                              #pip install whitenoise
+    default='postgresql://postgres:postgres@localhost/postgres',
+    conn_max_age=600
+    )
 }
 
 
@@ -117,6 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/signup/'         # redireccion de url cuando no se esta logiado--------------------------------------------------------
 
